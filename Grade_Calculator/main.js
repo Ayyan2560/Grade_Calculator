b0a74fa84628a6e236f67d4fd8722dbc40d88285
ayyan786920-max <ayyan786920@gnail.com>
2025-12-01 12:46:16 +0500

    code

--- Grade_Calculator/main.js
+++ Grade_Calculator/main.js
@@ -1,12 +1,16 @@
  document.getElementById('gradeForm').addEventListener('submit', function(event) {
             event.preventDefault();
             const score = parseFloat(document.getElementById('score').value);
-            let grade;
             if(score > 100){
-                grade = 'Invalid percentage';
-            }else if(score < 0){
-                grade = 'Invalid percentage';
-            } else if (score >= 80){
+            result.innerHTML = "⚠ Invalid percentage";
+            return;
+            }
+            else if(score < 0){
+            result.innerHTML = "⚠ Invalid percentage";
+            return;
+            }
+                let grade;
+             if (score >= 80){
                 grade = "A+"
             }else if (score >= 70){
                 grade = "A"
