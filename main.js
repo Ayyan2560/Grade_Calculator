6a672bbd0483fc859662876f99c9fdb82fa9ed17
Ayyan Rizwan <ayyan786920@gmail.com>
2025-12-01 14:04:57 +0500

    Add grade calculation functionality to main.js

---
+++ main.js
@@ -0,0 +1,30 @@
+ document.getElementById('gradeForm').addEventListener('submit', function(event) {
+            event.preventDefault();
+            const score = parseFloat(document.getElementById('score').value);
+            if(score > 100){
+            result.innerHTML = "⚠ Invalid percentage";
+            return;
+            }
+            else if(score < 0){
+            result.innerHTML = "⚠ Invalid percentage";
+            return;
+            }
+                let grade;
+             if (score >= 80){
+                grade = "A+"
+            }else if (score >= 70){
+                grade = "A"
+            }else if (score >= 60){
+                grade = "B"
+            }else if (score >= 50){
+                grade = "C"
+            }else if (score >= 40){
+                grade = "D"
+            } else if(score >= 33){
+                grade = "E"
+            }
+            else if (score <= 33){
+                grade = "FAIL"}
+                console.log(score , grade);
+            document.getElementById('result').textContent = `Your grade is: ${grade}`;
+        });
